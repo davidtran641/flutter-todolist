c1c5004568e2980de84edf30a26ab3018886787d
davidtran <davidtran641@gmail.com>
2020-08-13 09:27:02 +0800

    (TEST) Add  widget test

--- todo_redux/test/widget_test.dart
+++ todo_redux/test/widget_test.dart
@@ -14,5 +14,19 @@ void main() {
     await tester.pump();
 
     expect(find.byType(TextField), findsOneWidget);
+
+    // Add item 1
+    await tester.enterText(find.byType(TextField), "Item 1");
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pump();
+
+    expect(find.text('Item 1'), findsOneWidget);
+
+    // Remove item
+    await tester.tap(find.byIcon(Icons.delete));
+    await tester.pump();
+
+    expect(find.text('Item 1'), findsNothing);
+
   });
 }
