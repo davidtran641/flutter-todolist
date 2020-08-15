a4326da5ee4d463cc2e8f0fad948c5c02f8d8735
davidtran <davidtran641@gmail.com>
2020-08-15 22:17:07 +0800

    (TEST) Add more tests

--- todo_redux/test_driver/app_test.dart
+++ todo_redux/test_driver/app_test.dart
@@ -1,34 +1,67 @@
-
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
+Future<bool> isPresent(SerializableFinder finder, FlutterDriver driver,
+    {Duration timeout = const Duration(seconds: 1)}) async {
+  try {
+    await driver.waitFor(finder, timeout: timeout);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 void main() {
-        group('test add todo',  () {
-          final buttonFinder = find.byValueKey('floating_button');
-          final todoTextField = find.byValueKey('todo_text_field');
-          final saveButton = find.byValueKey('save_button');
-          final todoTitle = find.byValueKey('todo_text');
+  group('test add todo', () {
+    final todoTitle = find.byValueKey('todo_text');
+    final deleteButton = find.byValueKey('delete_button');
 
-          FlutterDriver driver;
-          
-          setUpAll(() async {
-            driver = await FlutterDriver.connect();
+    FlutterDriver driver;
 
-          });
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
 
-          tearDownAll(() async {
-            if(driver != null) {
-              driver.close();
-            }
-          });
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
 
-          test('Add  todo', () async {
-            await driver.tap(buttonFinder);
-            await driver.tap(todoTextField);
-            await driver.enterText('Item 1');
-            await driver.tap(saveButton);
+    Future<void> addItem(String text) async {
+      final buttonFinder = find.byValueKey('floating_button');
+      final todoTextField = find.byValueKey('todo_text_field');
+      final saveButton = find.byValueKey('save_button');
 
-            expect(await driver.getText(todoTitle), "Item 1");
-          });
+      await driver.tap(buttonFinder);
+      await driver.tap(todoTextField);
+      await driver.enterText(text);
+      await driver.tap(saveButton);
+    }
+
+    test('Add todo', () async {
+      // Add item
+      await addItem('Item 1');
+
+      expect(await driver.getText(todoTitle), "Item 1");
+
+      // Remove item
+      await driver.tap(deleteButton);
+
+      expect(await isPresent(todoTitle, driver, timeout: Duration(milliseconds: 100)), false);
+    });
+
+    test('Add todo 2', () async {
+      await addItem('Item 1');
+      await addItem('Item 2');
+      await addItem('Item 3');
+
+      expect(await isPresent(find.text('Item 1'), driver), true);
+      expect(await isPresent(find.text('Item 2'), driver), true);
+      expect(await isPresent(find.text('Item 3'), driver), true);
+      expect(await isPresent(find.text('Non existent item'), driver), false);
+    });
+
+
   });
 }
