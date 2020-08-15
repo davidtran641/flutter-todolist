0abb910f41d839859bafc90f18e422cce2fc942d
davidtran <davidtran641@gmail.com>
2020-08-15 22:55:13 +0800

    (TEST) Remove test text input

--- todo_redux/test_driver/app.dart
+++ todo_redux/test_driver/app.dart
@@ -4,17 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:todo_redux/main.dart' as app;
 
 void main() {
-  var textInput = TestTextInput();
-  Function(String) handler = (message) {
-    switch(message){
-      case 'done':
-        textInput.receiveAction(TextInputAction.done);
-        break;
-      default:
-        break;
-    }
-  };
-
-  enableFlutterDriverExtension(handler: handler);
+  enableFlutterDriverExtension();
   app.main();
 }
